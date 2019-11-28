af991e44944775fc68a88bcb41c8ed4c8a391e60
Ajay Kedia <akedia@C02W28FNHTD6.corp.proofpoint.com>
2019-11-28 10:49:23 -0700

    Unique path is created

---
+++ Coderpad/io/UniquePath.java
@@ -0,0 +1,34 @@
+/*
+find the unique path from top left cell to bottom right cell.
+ */
+
+package Coderpad.io;
+
+public class UniquePath
+{
+    public static void main(String args[]){
+        int m = 7; int n = 3;
+        UniquePath u = new UniquePath();
+        int uniquePath = u.getUniquePath(m, n);
+        System.out.println(uniquePath);
+    }
+
+    private int getUniquePath(int m, int n)
+    {
+        int[][] uniquePathMatrix = new int[m][n];
+        for(int i = 0; i < m; i++){
+            uniquePathMatrix[i][0] = 1;
+        }
+
+        for(int i = 0; i < n; i++){
+            uniquePathMatrix[0][i] = 1;
+        }
+
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                uniquePathMatrix[i][j] = uniquePathMatrix[i - 1][j] + uniquePathMatrix[i][j - 1];
+            }
+        }
+        return uniquePathMatrix[m -1][n-1];
+    }
+}
