e0b16c00ae319642dc2e7fc0f184c93736b6aee3
Ajay Kedia <akedia@C02W28FNHTD6.corp.proofpoint.com>
2019-11-28 14:14:21 -0700

    univalue tree question is added

---
+++ Coderpad/io/Tree.java
@@ -0,0 +1,14 @@
+package Coderpad.io;
+
+public class Tree
+{
+    int data;
+    Tree left;
+    Tree right;
+
+    Tree(int data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ Coderpad/io/UnivalueTree.java
@@ -0,0 +1,60 @@
+package Coderpad.io;
+
+public class UnivalueTree
+{
+    class UnivaluePackage{
+        int count;
+        boolean isUnival;
+        UnivaluePackage(int count, boolean isUnival){
+            this.count = count;
+            this.isUnival = isUnival;
+        }
+    }
+
+    public static void main(String args[]){
+        Tree root = new Tree(2);
+        root.left = new Tree(2);
+        root.right = new Tree(2);
+        root.right.left = new Tree(3);
+        root.right.right = new Tree(4);
+
+        int count = new UnivalueTree().getUnivalueTreeCount(root);
+        System.out.println(count);
+    }
+
+    private int getUnivalueTreeCount(Tree root)
+    {
+        if(root == null){
+            return 1;
+        }
+
+        UnivaluePackage up = getUnivalueTreeCountUtil(root);
+
+        return up.count;
+    }
+
+    private UnivaluePackage getUnivalueTreeCountUtil(Tree root)
+    {
+        if(root == null){
+            return new UnivaluePackage(0, true);
+        }
+
+        UnivaluePackage leftUnival = getUnivalueTreeCountUtil(root.left);
+        UnivaluePackage rightUnival = getUnivalueTreeCountUtil(root.right);
+        boolean isUnival = true;
+        if(!leftUnival.isUnival && !rightUnival.isUnival){
+            isUnival = false;
+        }
+        if(root.left != null && root.left.data != root.data){
+            isUnival = false;
+        }
+        if(root.right != null && root.right.data != root.data){
+            isUnival = false;
+        }
+        if(isUnival){
+            return new UnivaluePackage(leftUnival.count + rightUnival.count + 1, true);
+        }else{
+            return new UnivaluePackage(leftUnival.count + rightUnival.count,  false);
+        }
+    }
+}
