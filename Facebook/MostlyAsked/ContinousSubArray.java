77cd26abb2c0d338f7b50cf449f5bbc0892fe426
Ajay Kedia <akedia@C02W28FNHTD6.corp.proofpoint.com>
2019-12-19 19:53:49 -0700

    coding questions

---
+++ Facebook/MostlyAsked/ContinousSubArray.java
@@ -0,0 +1,44 @@
+package Facebook.MostlyAsked;
+
+public class ContinousSubArray
+{
+    public static void main(String args[]){
+        int arr[] = {-1, 3, 4, -6, 2, 5, -1};
+        ContinousSubArray c = new ContinousSubArray();
+        System.out.println(c.getContinuousSubArray(arr));
+    }
+
+    private int getContinuousSubArray(int[] arr)
+    {
+        int runningValue = 0;
+        int maxValue = Integer.MIN_VALUE;
+
+        for(int i : arr){
+            runningValue += i;
+
+            if(runningValue > maxValue){
+                maxValue = runningValue;
+            }
+            if(runningValue < 0){
+                runningValue = 0;
+            }
+        }
+        return maxValue;
+    }
+
+    public int getContinuousSubArray1(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int[] temp = new int[nums.length];
+
+        temp[0] = nums[0];
+        int maxValue = temp[0];
+        for(int i = 1; i < nums.length; i++){
+            temp[i] = Math.max(nums[i], nums[i] + temp[i - 1]);
+            maxValue = Math.max(maxValue, temp[i]);
+        }
+        return maxValue;
+    }
+}
---
+++ Facebook/MostlyAsked/CustomSortString.java
@@ -0,0 +1,67 @@
+package Facebook.MostlyAsked;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class CustomSortString
+{
+    public static void main(String args[]){
+
+        String S = "cba";
+        String T = "abcd";
+        CustomSortString c = new CustomSortString();
+        System.out.println(c.customSortString(S, T));
+
+    }
+    public String customSortString(String S, String T) { //2ms
+        Map<Character, Integer> map = new HashMap<>();
+
+        for(char c : T.toCharArray()){
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < S.length(); i++){
+            char c = S.charAt(i);
+            if(map.containsKey(c)){
+                for(int j = 0; j < map.get(c); j++){
+                    sb.append(c);
+                }
+                map.remove(c);
+            }
+        }
+
+        Set<Character> keys = map.keySet();
+        for(char c : keys){
+            int values = map.get(c);
+            for(int j = 0; j < values; j++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+
+    public String customSortString1(String S, String T) {  // 0ms
+        int[] character = new int[26];
+        for(char c : T.toCharArray()){
+            character[c - 'a']++;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < S.length(); i++){
+            char c = S.charAt(i);
+            for(int j = 0; j < character[c - 'a']; j++){
+                sb.append(c);
+            }
+            character[c - 'a'] = 0;
+        }
+
+        for(char c = 'a'; c <= 'z'; c++){
+            for(int j = 0; j < character[c - 'a']; j++){
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
