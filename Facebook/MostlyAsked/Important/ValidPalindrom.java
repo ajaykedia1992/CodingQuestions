5d09c8aa9022637ec825257f17a8913480ed97f5
Ajay Kedia <akedia@C02W28FNHTD6.corp.proofpoint.com>
2019-12-25 14:54:56 -0700

    valid palindrome description

--- Facebook/MostlyAsked/Important/ValidPalindrom.java
+++ Facebook/MostlyAsked/Important/ValidPalindrom.java
@@ -1,3 +1,15 @@
+/*
+https://leetcode.com/problems/valid-palindrome-ii/
+Given a non-empty string s, you may delete at most one character. Judge whether you can make it a palindrome.
+
+Example 1:
+Input: "aba"
+Output: True
+Example 2:
+Input: "abca"
+Output: True
+Explanation: You could delete the character 'c'.
+ */
 package Facebook.MostlyAsked.Important;
 
 public class ValidPalindrom
