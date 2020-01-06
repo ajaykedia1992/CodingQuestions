078af819c536335d1ff5beaed7c456fbb430616a
Ajay Kedia <akedia@C02W28FNHTD6.corp.proofpoint.com>
2020-01-06 09:16:11 -0800

    optimal strategy

--- DynamicProgramming/OptimalGameStrategy.java
+++ DynamicProgramming/OptimalGameStrategy.java
@@ -1,3 +1,13 @@
+/*
+https://www.geeksforgeeks.org/optimal-strategy-for-a-game-set-2/
+Consider a row of n coins of values v1 . . . vn, where n is even. We play a game against an opponent by alternating turns.
+In each turn, a player selects either the first or last coin from the row, removes it from the row permanently, and receives the value of the coin.
+Determine the maximum possible amount of money we can definitely win if we move first.
+Note: The opponent is as clever as the user.
+Let us understand the problem with few examples:
+1. 5, 3, 7, 10 : The user collects maximum value as 15(10 + 5)
+2. 8, 15, 3, 7 : The user collects maximum value as 22(7 + 15)
+ */
 package DynamicProgramming;
 
 public class OptimalGameStrategy
